5a56e7579652f527e2f0239a807e3c047ff85996
andrew <andrewrogov@mail.ru>
2021-06-07 12:51:17 +0500

    Решение 2390 (19-21) рекурсией

--- 19-21/2390 recursion.py
+++ 19-21/2390 recursion.py
@@ -1,51 +1,51 @@
 # 2 кучи
 # +1 *2
-# 4, 1 <= s <= 40
-# win 45
+# 8, 1 <= s <= 66
+# win 75
+win = 75
+end = 67
+first = 8
+d = {}
 
 
 def turn(x, y):
     return (x + 1, y), (x * 2, y), (x, y + 1), (x, y * 2)
 
 
-def win(h):
-    return sum(h) >= 75
+def f(x, y):
+    if (x, y) in d:
+        return d[(x, y)]
+    # 0 - признак победы
+    if x + y >= win:
+        d[(x, y)] = 0
+        return 0
+    # хотя бы один ход победный за 1 ход
+    if any(f(i, j) == 0 for i, j in turn(x, y)):
+        d[(x, y)] = 1
+        return 1
+    # для 19 any (не выигрышная стратегия)
+    if all(f(i, j) == 1 for i, j in turn(x, y)):
+        d[(x, y)] = 2
+        return 2
+    if any(f(i, j) == 2 for i, j in turn(x, y)):
+        d[(x, y)] = 3
+        return 3
+    if all(f(i, j) in (1, 3) for i, j in turn(x, y)):
+        d[(x, y)] = 4
+        return 4
+    d[(x, y)] = -1
+    return -1
 
 
-s = 67
-pos = []
-for i in range(500):
-    row = []
-    for j in range(500):
-        row.append(0)
-    pos.append(row)
-# pos = [[0 for _ in range(500)] for _ in range(500)]
-
-# победа в 1 ход
-for x in range(s):
-    for y in range(s):
-        if any(win(h) for h in turn(x, y)):
-            pos[x][y] = 1
-
-# поражение в 1 ход
-for x in range(s):
-    for y in range(s):
-        if pos[x][y] == 0:
-            if all(pos[h[0]][h[1]] == 1 for h in turn(x, y)):
-                pos[x][y] = -1
-
-# победа в 2 хода
-for x in range(s):
-    for y in range(s):
-        if pos[x][y] == 0:
-            if any(pos[h[0]][h[1]] == -1 for h in turn(x, y)):
-                pos[x][y] = 2
-
-# поражение в 2 хода
-for x in range(s):
-    for y in range(s):
-        if pos[x][y] == 0:
-            if all(pos[h[0]][h[1]] in (1, 2) for h in turn(x, y)):
-                pos[x][y] = -2
-
-print(*enumerate(pos[8][:s]), sep='\n')
+# # 19
+# for s in range(1, end):
+#     if f(first, s) == 2:
+#         print(19, s, sep='\t')
+# 20
+# for s in range(1, end):
+#     if f(first, s) == 3:
+#         print(20, s, sep='\t')
+# 21
+for s in range(1, end):
+    if f(first, s) == 4:
+        print(21, s, sep='\t')
