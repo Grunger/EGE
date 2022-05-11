bd74556a88cf3b49b8b7f507491a0c119d404a2f
andrew <andrewrogov@mail.ru>
2022-05-11 21:22:55 +0500

    Исправлены решения задач 19-21. Добавлено два новых решения.

--- kpolyakov.spb.ru/19-21/4735.py
+++ kpolyakov.spb.ru/19-21/4735.py
@@ -1,13 +1,12 @@
-def move(h):
-    a, b = h
-    return (a, b + 1), (a, b * 2), (a + 1, b), (a * 2, b)
+def move(a, b):
+    return (a, b + 2), (a, b * 2), (a + 2, b), (a * 2, b)
 
 
-size = 500
-s = [[0] * size for _ in range(size)]
-win = 91  # количество камней для победы (включительно)
-lose = 110  # количество камней для поражения
-first = 40  # количество камней в первой куче
+size = 100
+s = [[0] * 1000 for _ in range(1000)]
+win = 63  # количество камней для победы (включительно)
+lose = 74 + 1  # количество камней для поражения
+first = 15  # количество камней в первой куче
 # где победа. Если сумма камней в требуемом диапазоне
 for i in range(size):
     for j in range(size):
@@ -16,25 +15,31 @@ for i in range(size):
 # победа первым ходом. Если есть ход из неопределенной позиции в победную
 for i in range(size):
     for j in range(size):
-        if s[i][j] == 0 and any(s[a][b] == 5 for a, b in move((i, j))):
+        if s[i][j] == 0 and any(s[a][b] == 5 for a, b in move(i, j)):
             s[i][j] = 1
-# проигрышные позиции за 1 ход. Если все ходы ведут либо в проигрышную позицию, либо перепрыгивают зону победы
+# проигрышные позиции за 1 ход. Если все ходы ведут либо в проигрышную позицию, либо перепрыгивают
+# зону победы
 for i in range(size):
     for j in range(size):
-        if s[i][j] == 0 and all(s[a][b] == 1 or a + b >= lose for a, b in move((i, j))):
+        if s[i][j] == 0 and i + j < win and all(s[a][b] == 1 or a + b >= lose for a, b in move(i, j)):
             s[i][j] = -1
         # print(i, j)
 # выигрышные позиции за 2 хода. Если есть ход в проигрышную позицию и она в пределах игровых позиций
 for i in range(size):
     for j in range(size):
-        if s[i][j] == 0 and any(s[a][b] == -1 and a + b < win for a, b in move((i, j))):
+        if s[i][j] == 0 and any(s[a][b] == -1 for a, b in move(i, j)):
             s[i][j] = 2
 # проигрышные позиции за 2 хода. Если все ходы либо в выигрышные позиции, либо в зону поражения
 for i in range(size):
     for j in range(size):
-        if s[i][j] == 0 and all(s[a][b] in (1, 2) or a + b >= lose for a, b in move((i, j))):
+        if s[i][j] == 0 and i + j < win and all(s[a][b] in (1, 2) or a + b >= lose for a, b in move(i, j)):
             s[i][j] = -2
 
 # вывод начальных ситуаций
-for i in range(win - first):
+for i in range(win - first + 1):
     print(i, s[first][i])
+
+# for i in range(size):
+#     for j in range(size):
+#         print(s[i][j], end='\t')
+#     print()
