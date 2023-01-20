61c2513359d3d4164f759bee8dd6b3d089784f1f
andrew <andrewrogov@mail.ru>
2023-01-20 13:01:55 +0500

    Удалены лишние файлы

--- Пробные/2023/12.2022/5 (2).py
+++
@@ -1,9 +0,0 @@
-for i in range(1, 15):
-    n = bin(i)[2:]
-    if n.count('1') % 2 == 0:
-        n = n + '1'
-        n = '10' + n[2:]
-    else:
-        n = n + '0'
-        n = '11' + n[2:]
-    print(i, int(n, 2))
--- Пробные/2023/12.2022/8 (2).py
+++
@@ -1,9 +0,0 @@
-from itertools import product
-
-k = 0
-for item in product('АРБУЗ', repeat=6):
-    if item.count('А') == 3 and 'АА' in ''.join(item) and 'ААА' not in ''.join(item):
-        k += 1
-print(k)
-
-print(4**3 * 3 * 2 + 4**3 * 2 * 3)
