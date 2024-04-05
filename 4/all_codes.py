dc1046f7c2ce5e18afabf0f3e9b8973938365d25
andrew <andrewrogov@mail.ru>
2024-04-05 08:33:57 +0500

    Исправление ошибок

---
+++ 4/all_codes.py
@@ -0,0 +1,15 @@
+def check_fano(code, codes):
+    return all(code != c[:len(code)] for c in codes)
+
+
+def next_code(code):
+    return code + '0', code + '1'
+
+
+max_len = 5
+n = 10
+first = ''
+for i in range(2**max_len):
+    codes = []
+    for code in next_code(first)
+
--- Silvertests/26/Книжные полки/2/26.py
+++ Silvertests/26/Книжные полки/2/26.py
@@ -1,16 +1,16 @@
-f = open('26_books_test.txt')
+f = open('26_books.txt')
 n, s = map(int, f.readline().split())
 a = []
 for _ in range(n):
     i, j = map(int, f.readline().split())
     a.append((i + 10, j))
 a.sort(key=lambda x: (-x[1], x[0]))
-
+print(a)
 i = 0
 sm = 0
-while a[i][1] == 1:
-    sm += a[i][0]
-    i += 1
+# while a[i][1] == 1:
+#     sm += a[i][0]
+#     i += 1
 while sm <= s:
     sm += a[i][0]
     i += 1
